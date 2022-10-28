07cd765a87ab6065480479e2bec27c964495b7c4
b-van-b <115096508+b-van-b@users.noreply.github.com>
2022-10-28 15:37:51 +0000

    chore: use common callback in api routes

--- routes/api.js
+++ routes/api.js
@@ -8,6 +8,16 @@
 
 "use strict";
 
+const callback = (res) => {
+  return (err, data) => {
+    if (err) {
+      console.log(err);
+      return res.json(err);
+    }
+    return res.json(data);
+  };
+};
+
 module.exports = function (app, models) {
   // get Book model from database
   const Book = models.Book;
@@ -17,25 +27,13 @@ module.exports = function (app, models) {
     .get(function (req, res) {
       //response will be array of book objects
       //json res format: [{"_id": bookid, "title": book_title, "commentcount": num_of_comments },...]
-      Book.getCollectionSummary((err, data) => {
-        if (err) {
-          console.log(err);
-          return res.json(err);
-        }
-        return res.json(data);
-      });
+      Book.getCollectionSummary(callback(res));
     })
 
     .post(function (req, res) {
       let title = req.body.title;
       //response will contain new book object including atleast _id and title
-      Book.addOne(title, (err, data) => {
-        if (err) {
-          console.log(err);
-          return res.json(err);
-        }
-        return res.json(data);
-      });
+      Book.addOne(title, callback(res));
     })
 
     .delete(function (req, res) {
@@ -47,26 +45,14 @@ module.exports = function (app, models) {
     .get(function (req, res) {
       let bookid = req.params.id;
       //json res format: {"_id": bookid, "title": book_title, "comments": [comment,comment,...]}
-      Book.getOne(bookid, (err, data) => {
-        if (err) {
-          console.log(err);
-          return res.json(err);
-        }
-        return res.json(data);
-      });
+      Book.getOne(bookid, callback(res));
     })
 
     .post(function (req, res) {
       let bookid = req.params.id;
       let comment = req.body.comment;
       //json res format same as .get
-      Book.addComment(bookid, comment, (err, data) => {
-        if (err) {
-          console.log(err);
-          return res.json(err);
-        }
-        return res.json(data);
-      });
+      Book.addComment(bookid, comment, callback(res));
     })
 
     .delete(function (req, res) {
