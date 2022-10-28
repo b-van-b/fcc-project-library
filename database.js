b5b9565b163e2465bc2a2374ca56cbdfb1153f50
b-van-b <115096508+b-van-b@users.noreply.github.com>
2022-10-28 14:00:53 +0000

    chore: create basic db connection function and book schema

---
+++ database.js
@@ -0,0 +1,34 @@
+"use strict";
+require("dotenv").config();
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// initiate connection to database
+const connect = () => {
+  console.log("Attempting to connect to database...");
+  return new Promise((resolve, reject) => {
+    mongoose
+      .connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      })
+      .then(() => {
+        resolve("Connected to database!");
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
+};
+
+// schemas
+const Book = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  comments: [String],
+});
+
+// exports
+module.exports = { connect, models: { Book } };
